5bae366a318df96df4611c8b47be3906ff4bbf07
Maximilian Hildebrand <github@m10x.de>
2022-07-08 15:53:19 +0200

    improved edit row

--- main.go
+++ main.go
@@ -17,9 +17,12 @@ var data = [][]string{header}
 var strNoDevice = "No Device Found"
 var strStartListen = "Start Listen"
 var strStopListen = "Stop Listen"
-var midiDevice = strNoDevice
-var combo *widget.Select
+var comboSelect *widget.Select
+var comboHotkey *widget.Select
 var btnListen *widget.Button
+var btnAddRow *widget.Button
+var btnDeleteRow *widget.Button
+var btnEditRow *widget.Button
 var selectedCell widget.TableCellID
 
 func refreshDevices() {
@@ -27,8 +30,8 @@ func refreshDevices() {
 	if len(devices) == 0 {
 		devices = append(devices, strNoDevice)
 	}
-	combo.Options = devices
-	combo.SetSelectedIndex(0)
+	comboSelect.Options = devices
+	comboSelect.SetSelectedIndex(0)
 }
 
 func main() {
@@ -38,10 +41,9 @@ func main() {
 
 	hello := widget.NewLabel("Hello! :)")
 
-	combo = widget.NewSelect([]string{""}, func(value string) {
-		midiDevice = value
-		fmt.Sprintln("Selected midi device " + midiDevice)
-		if combo.Selected != strNoDevice {
+	comboSelect = widget.NewSelect([]string{""}, func(value string) {
+		fmt.Sprintln("Selected midi device " + value)
+		if comboSelect.Selected != strNoDevice {
 			btnListen.Enable()
 		} else {
 			btnListen.Disable()
@@ -54,22 +56,28 @@ func main() {
 
 	btnListen = widget.NewButton(strStartListen, func() {
 		if btnListen.Text == strStartListen {
-			startListen(midiDevice)
+			startListen(comboSelect.Selected)
 			btnListen.Text = strStopListen
 			btnListen.Refresh()
 			btnRefresh.Disable()
-			combo.Disable()
+			comboSelect.Disable()
+			btnAddRow.Disable()
+			btnDeleteRow.Disable()
+			btnEditRow.Disable()
 
 		} else {
 			stopListen()
 			btnListen.Text = strStartListen
 			btnListen.Refresh()
 			btnRefresh.Enable()
-			combo.Enable()
+			comboSelect.Enable()
+			btnAddRow.Enable()
+			btnDeleteRow.Enable()
+			btnEditRow.Enable()
 		}
 	})
 
-	hBoxSelect := container.NewHBox(hello, combo, btnRefresh, btnListen)
+	hBoxSelect := container.NewHBox(btnRefresh, btnListen)
 
 	table := widget.NewTable(
 		func() (int, int) {
@@ -87,27 +95,12 @@ func main() {
 		fmt.Println("Selected Cell Col", selectedCell.Col, "Row", selectedCell.Row)
 	}
 
-	btnAddRow := widget.NewButton("Add Row", func() {
+	btnAddRow = widget.NewButton("Add Row", func() {
 		dataLen := strconv.Itoa(len(data))
 		data = append(data, []string{dataLen, dataLen, dataLen, dataLen})
 		table.Refresh()
 	})
-	btnEditRow := widget.NewButton("Edit Row", func() {
-		popupEdit := a.NewWindow("Edit Row")
-		popupEdit.Resize(fyne.NewSize(400, 300))
-
-		lblNote := widget.NewLabel("Note:")
-		btnNote := widget.NewButton("Press this Button", nil)
-		lblDescription := widget.NewLabel("Description:")
-		entryDescription := widget.NewEntry()
-
-		btnSave := widget.NewButton("Save", nil)
-		btnCancel := widget.NewButton("Cancel", nil)
-
-		popupEdit.SetContent(container.NewVBox(container.New(layout.NewFormLayout(), lblNote, btnNote, lblDescription, entryDescription), container.NewCenter(container.NewHBox(btnSave, btnCancel))))
-		popupEdit.Show()
-	})
-	btnDeleteRow := widget.NewButton("Delete Row", func() {
+	btnDeleteRow = widget.NewButton("Delete Row", func() {
 		tmpData := [][]string{header}
 		for i, x := range data {
 			if i != selectedCell.Row && i != 0 { // Dont apped header again, dont append row to delete
@@ -117,11 +110,70 @@ func main() {
 		data = tmpData
 		table.Refresh()
 	})
+	btnEditRow = widget.NewButton("Edit Row", func() {
+		if selectedCell.Row == 0 {
+			return
+		}
+		btnAddRow.Disable()
+		btnDeleteRow.Disable()
+		btnEditRow.Disable()
+		comboSelect.Disable()
+		btnRefresh.Disable()
+		btnListen.Disable()
+
+		rowToEdit := selectedCell.Row
+		popupEdit := a.NewWindow("Edit Row")
+		popupEdit.Resize(fyne.NewSize(400, 300))
+		popupEdit.SetOnClosed(func() {
+			btnAddRow.Enable()
+			btnDeleteRow.Enable()
+			btnEditRow.Enable()
+			btnListen.Enable()
+			if btnListen.Text == strStartListen {
+				comboSelect.Enable()
+				btnRefresh.Enable()
+			}
+		})
+
+		lblNote := widget.NewLabel("Note:")
+		btnNote := widget.NewButton("Press this Button", nil)
+		lblDescription := widget.NewLabel("Description:")
+		entryDescription := widget.NewEntry()
+		lblHotkey := widget.NewLabel("Hotkey:")
+		entryHotkey := widget.NewEntry()
+		comboHotkey = widget.NewSelect([]string{"Command Line Command", "Keypress (combo)", "Audio Control"}, func(value string) {
+			if comboHotkey.SelectedIndex() == -1 {
+				entryHotkey.Disable()
+			} else {
+				entryHotkey.Enable()
+				// TODO: Add Popup to speficy the wanted hotkey. Eg. if Audio Control: Popup to choose if Mute, Volume Up, Volume Down
+			}
+		})
+		comboHotkey.SetSelectedIndex(0)
+
+		btnSave := widget.NewButton("Save", func() {
+			data[rowToEdit][0] = btnNote.Text
+			data[rowToEdit][1] = entryHotkey.Text
+			data[rowToEdit][2] = entryDescription.Text
+			table.Refresh()
+			popupEdit.Close()
+		})
+		btnCancel := widget.NewButton("Cancel", func() {
+			popupEdit.Close()
+		})
+
+		btnNote.Text = data[rowToEdit][0]
+		entryHotkey.Text = data[rowToEdit][1]
+		entryDescription.Text = data[rowToEdit][2]
+
+		popupEdit.SetContent(container.NewVBox(container.New(layout.NewFormLayout(), lblNote, btnNote, lblHotkey, container.NewVBox(comboHotkey, entryHotkey), lblDescription, entryDescription), container.NewCenter(container.NewHBox(btnSave, btnCancel))))
+		popupEdit.Show()
+	})
 
 	hBoxTable := container.NewHBox(btnAddRow, btnEditRow, btnDeleteRow)
 
 	w.SetContent(container.NewBorder(
-		hBoxSelect, hBoxTable, nil, nil,
+		container.NewBorder(nil, nil, hello, hBoxSelect, comboSelect), hBoxTable, nil, nil,
 		table,
 	))
 
