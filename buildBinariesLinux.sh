6b25767b24b94f076587af074fa9819223a7ef4a
Maximilian Hildebrand <github@m10x.de>
2023-12-26 08:07:45 +0100

    fix buildscript

--- buildBinariesLinux.sh
+++ buildBinariesLinux.sh
@@ -8,46 +8,32 @@ mkdir build
 # Windows amd64
 goos=windows
 goarch=amd64
-set GOOS=$goos
-set GOARCH=$goarch
-go build -buildmode=exe -o "$dir"/"$tool".exe
+GOOS=$goos GOARCH=$goarch go build -buildmode=exe -o "$dir"/"$tool".exe
 zip build/"$tool"_"$version"_"$goos"_"$goarch".zip "$dir"/"$tool".exe
 
 # Linux amd64
 goos=linux
 goarch=amd64
-set GOOS=$goos
-set GOARCH=$goarch
-go build -o "$dir"/"$tool"
+GOOS=$goos GOARCH=$goarch go build -o "$dir"/"$tool"
 zip build/"$tool"_"$version"_"$goos"_"$goarch".zip "$dir"/"$tool"
 
 # Linux arm64
 goos=linux
 goarch=arm64
-set GOOS=$goos
-set GOARCH=$goarch
-go build -o "$dir"/"$tool"
+GOOS=$goos GOARCH=$goarch go build -o "$dir"/"$tool"
 zip build/"$tool"_"$version"_"$goos"_"$goarch".zip "$dir"/"$tool"
 
 # Darwin/MacOS amd64
 goos=darwin
 goarch=amd64
-set GOOS=$goos
-set GOARCH=$goarch
-go build -o "$dir"/"$tool"
+GOOS=$goos GOARCH=$goarch go build -o "$dir"/"$tool"
 zip build/"$tool"_"$version"_"$goos"_"$goarch".zip "$dir"/"$tool"
 
 # Darwin/MacOS arm64
 goos=darwin
 goarch=arm64
-set GOOS=$goos
-set GOARCH=$goarch
-go build -o "$dir"/"$tool"
+GOOS=$goos GOARCH=$goarch go build -o "$dir"/"$tool"
 zip build/"$tool"_"$version"_"$goos"_"$goarch".zip "$dir"/"$tool"
-
-# reset GOOS and GOARCH
-set GOOS=
-set GOARCH=
 
 # remove wcvs
 rm "$dir"/"$tool"
