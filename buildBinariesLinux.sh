b41e631b9cc103993b621860b8064d02d15b295d
Maximilian Hildebrand <github@m10x.de>
2025-01-02 23:59:19 +0100

    update build script

--- buildBinariesLinux.sh
+++ buildBinariesLinux.sh
@@ -1,43 +1,42 @@
 #!/bin/bash
-tool=midi2key-ng
 version=1.2.0
-dir=build
 
-mkdir build
+mkdir build/
+rm build/*
 
 # Windows amd64
 goos=windows
 goarch=amd64
 GOOS=$goos GOARCH=$goarch go build -o midi2key-ng.exe
-zip build/"$tool"_"$version"_"$goos"_"$goarch".zip midi2key-ng.exe
+zip build/midi2key-ng_"$version"_"$goos"_"$goarch".zip midi2key-ng.exe
 
 # Linux amd64
 goos=linux
 goarch=amd64
 GOOS=$goos GOARCH=$goarch go build -o midi2key-ng
-zip build/"$tool"_"$version"_"$goos"_"$goarch".zip midi2key-ng
+zip build/midi2key-ng_"$version"_"$goos"_"$goarch".zip midi2key-ng
 
 # Linux arm64
 goos=linux
 goarch=arm64
 GOOS=$goos GOARCH=$goarch go build -o midi2key-ng
-zip build/"$tool"_"$version"_"$goos"_"$goarch".zip midi2key-ng
+zip build/midi2key-ng_"$version"_"$goos"_"$goarch".zip midi2key-ng
 
 # Darwin/MacOS amd64
 goos=darwin
 goarch=amd64
 GOOS=$goos GOARCH=$goarch go build -o midi2key-ng
-zip build/"$tool"_"$version"_"$goos"_"$goarch".zip midi2key-ng
+zip build/midi2key-ng_"$version"_"$goos"_"$goarch".zip midi2key-ng
 
 # Darwin/MacOS arm64
 goos=darwin
 goarch=arm64
 GOOS=$goos GOARCH=$goarch go build -o midi2key-ng
-zip build/"$tool"_"$version"_"$goos"_"$goarch".zip midi2key-ng
+zip build/midi2key-ng_"$version"_"$goos"_"$goarch".zip midi2key-ng
 
 # remove wcvs
 rm midi2key-ng
 rm midi2key-ng.exe
 
 # generate checksum file
-find "$dir"/ -type f  \( -iname "*.zip" \) -exec sha256sum {} + > "$dir"/"$tool"_"$version"_checksums_sha256.txt
+find build/ -type f  \( -iname "*.zip" \) -exec sha256sum {} + > build/midi2key-ng_"$version"_checksums_sha256.txt
